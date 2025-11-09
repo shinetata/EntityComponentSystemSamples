871d5f82a6bbe19e5ac728aa31982705aba2502f
SunQi <sunqfight@outlook.com>
2025-11-09 11:38:02 +0800

    改造

--- Dots101/Entities101/Assets/HelloCube/12. FixedTimestep/MoveProjectilesSystem.cs
+++ Dots101/Entities101/Assets/HelloCube/12. FixedTimestep/MoveProjectilesSystem.cs
@@ -36,3 +36,4 @@ namespace HelloCube.FixedTimestep
         }
     }
 }
+      
--- Dots101/Entities101/Assets/HelloCube/13. CustomTransforms/LocalToWorld2DSystem.cs
+++ Dots101/Entities101/Assets/HelloCube/13. CustomTransforms/LocalToWorld2DSystem.cs
@@ -18,57 +18,60 @@ namespace HelloCube.CustomTransforms
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            var childRelations = SystemAPI.QueryBuilder()
+            var childQuery = SystemAPI.QueryBuilder()
                 .WithAll<LocalTransform2D, Parent>()
                 .Build();
 
-            int childCount = childRelations.CalculateEntityCount();
-            var childMap = new NativeParallelMultiHashMap<Entity, Entity>(childCount, Allocator.Temp);
+            var rootsQuery = SystemAPI.QueryBuilder()
+                .WithAll<LocalTransform2D>()
+                .WithNone<Parent>()
+                .Build();
 
-            foreach (var (parent, entity) in SystemAPI.Query<RefRO<Parent>>().WithAll<LocalTransform2D>().WithEntityAccess())
+            int estimatedChildCount = math.max(childQuery.CalculateEntityCount(), 1);
+            var childMap = new NativeParallelMultiHashMap<int, Entity>(estimatedChildCount, Allocator.Temp);
+
+            foreach (var (parent, entity) in
+                     SystemAPI.Query<RefRO<Parent>>()
+                         .WithAll<LocalTransform2D>()
+                         .WithEntityAccess())
             {
-                childMap.Add(parent.ValueRO.Value, entity);
+                childMap.Add(parent.ValueRO.Value.Index, entity);
             }
 
             var stack = new NativeList<StackItem>(Allocator.Temp);
-            var roots = SystemAPI.QueryBuilder()
-                .WithAll<LocalTransform2D>()
-                .WithNone<Parent>()
-                .Build()
-                .ToEntityArray(Allocator.Temp);
+            var roots = rootsQuery.ToEntityArray(Allocator.Temp);
 
-            for (int i = 0; i < roots.Length; i++)
+            foreach (var root in roots)
             {
                 stack.Add(new StackItem
                 {
-                    Entity = roots[i],
+                    Entity = root,
                     ParentMatrix = float4x4.identity
                 });
             }
 
-            var iterator = default(NativeParallelMultiHashMapIterator<Entity>);
             while (stack.Length > 0)
             {
                 int last = stack.Length - 1;
-                var item = stack[last];
+                var current = stack[last];
                 stack.RemoveAtSwapBack(last);
 
-                if (!SystemAPI.HasComponent<LocalTransform2D>(item.Entity))
+                if (!SystemAPI.HasComponent<LocalTransform2D>(current.Entity))
                     continue;
 
-                var transform2D = SystemAPI.GetComponent<LocalTransform2D>(item.Entity);
+                var transform2D = SystemAPI.GetComponent<LocalTransform2D>(current.Entity);
                 float4x4 localMatrix = transform2D.ToMatrix();
 
-                if (SystemAPI.HasComponent<PostTransformMatrix>(item.Entity))
+                if (SystemAPI.HasComponent<PostTransformMatrix>(current.Entity))
                 {
-                    var postTransform = SystemAPI.GetComponent<PostTransformMatrix>(item.Entity);
+                    var postTransform = SystemAPI.GetComponent<PostTransformMatrix>(current.Entity);
                     localMatrix = math.mul(localMatrix, postTransform.Value);
                 }
 
-                var worldMatrix = math.mul(item.ParentMatrix, localMatrix);
-                SystemAPI.SetComponent(item.Entity, new LocalToWorld { Value = worldMatrix });
+                var worldMatrix = math.mul(current.ParentMatrix, localMatrix);
+                SystemAPI.SetComponent(current.Entity, new LocalToWorld { Value = worldMatrix });
 
-                if (childMap.TryGetFirstValue(item.Entity, out var child, out iterator))
+                if (childMap.TryGetFirstValue(current.Entity.Index, out var child, out var iterator))
                 {
                     do
                     {
@@ -77,13 +80,14 @@ namespace HelloCube.CustomTransforms
                             Entity = child,
                             ParentMatrix = worldMatrix
                         });
-                    } while (childMap.TryGetNextValue(out child, ref iterator));
+                    }
+                    while (childMap.TryGetNextValue(out child, ref iterator));
                 }
             }
 
+            roots.Dispose();
             stack.Dispose();
             childMap.Dispose();
-            roots.Dispose();
         }
 
         struct StackItem
